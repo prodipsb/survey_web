df8d16df9b2f88c6a04b817f4b1b1ec212e94be0
prodipsb <prodip5080@gmail.com>
2023-11-23 11:25:21 +0600

    git ignore update

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: ["4.193.55.34"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
